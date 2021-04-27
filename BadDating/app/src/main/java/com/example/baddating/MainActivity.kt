c099a868cda7cb80e3321935426a134d81d2640c
Dallen Aquino <42020676+dallenaquino@users.noreply.github.com>
2021-04-27 11:09:22 -0600

    Added default LoginActivity

--- BadDating/app/src/main/java/com/example/baddating/MainActivity.kt
+++ BadDating/app/src/main/java/com/example/baddating/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.baddating
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.baddating.ui.login.LoginActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
     }
 }
